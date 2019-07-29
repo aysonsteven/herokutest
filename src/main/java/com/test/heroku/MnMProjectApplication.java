4d77dae574c138d155339f55bafe7df4064e5fda
Mark Steven <aysonsteven@gmail.com>
2019-07-29 13:33:29 +0800

    Initial commit

---
+++ src/main/java/com/test/heroku/MnMProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnMProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnMProjectApplication.class, args);
+	}
+
+}
