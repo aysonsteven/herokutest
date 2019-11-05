004222fba0cacda13b460483b4724429e3e71118
aysonsteven <aysonsteven@gmail.com>
2019-11-05 17:12:59 +0800

    Update MnMProjectApplication.java

--- src/main/java/com/test/heroku/MnMProjectApplication.java
+++ src/main/java/com/test/heroku/MnMProjectApplication.java
@@ -2,12 +2,22 @@ package com.test.heroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RequestMapping( produces = "application/json")
+@RestController
 public class MnMProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MnMProjectApplication.class, args);
+	}
+	
+	@GetMapping
+	public String test(){
+		return "Success";
 	}
 
 }
